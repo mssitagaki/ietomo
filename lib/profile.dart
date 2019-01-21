459ae1cf3aabb72d17e64dad5a2801016c690913
Masashiiii <>
2019-01-22 01:59:35 +0900

    chat&profile

---
+++ lib/profile.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfilePage extends StatefulWidget {
+  ChatPage(this._userID);
+
+  final String _userID;
+
+  @override
+  _ProfilePageState createState() => new _PhatPageState();
+}
+
+class _PhatPageState extends State<ChatPage> {
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("プロフィール画面"),
+        ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: Firestore.instance.collection('users')
+                      .document(widget._userID)
+                      .collection("Profile")
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return Container();
+                    return new ListView.builder(
+                      padding: new EdgeInsets.all(8.0),
+                      reverse: true,
+                      itemBuilder: (_, int index) {
+                        DocumentSnapshot document =
+                        snapshot.data.documents[index];
+
+                        bool isOwnMessage = false;
+                        if (document['user_name'] == widget._userID) {
+                          isOwnMessage = true;
+                        }
+                        return isOwnMessage
+                            ? _ownMessage(
+                            document['message'], document['user_name'])
+                            : _message(
+                            document['message'], document['user_name']);
+                      },
+                      itemCount: snapshot.data.documents.length,
+                    );
+                  },
+                ),
+              ),
+              new Divider(height: 1.0),
+              Container(
+                margin: EdgeInsets.only(bottom: 20.0, right: 10.0, left: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    new Flexible(
+                      child: new TextField(
+                        controller: _controller,
+                        onSubmitted: _handleSubmit,
+                        decoration:
+                        new InputDecoration.collapsed(hintText: "メッセージの送信"),
+                      ),
+                    ),
+                    new Container(
+                      child: new IconButton(
+                          icon: new Icon(
+                            Icons.send,
+                            color: Colors.blue,
+                          ),
+                          onPressed: () {
+                            _handleSubmit(_controller.text);
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget _ownMessage(String message, String userName) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10.0,),
+            Text(userName),
+            Text(message),
+          ],
+        ),
+        Icon(Icons.person),
+      ],
+    );
+  }
+
+  Widget _message(String message, String userName) {
+    return Row(
+      children: <Widget>[
+        Icon(Icons.person),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10.0,),
+            Text(userName),
+            Text(message),
+          ],
+        )
+      ],
+    );
+  }
+
+  _handleSubmit(String message) {
+    _controller.text = "";
+    var db = Firestore.instance;
+    db.collection('users').document(widget._userID).collection("Profile").add({
+      "userID: widget._userID,
+      "message": message,
+      "created_at": DateTime.now()
+    }).then((val) {
+      print("成功です");
+    }).catchError((err) {
+      print(err);
+    });
+  }
+}
